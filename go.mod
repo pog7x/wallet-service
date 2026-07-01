bafcaa4c6c89fdb15c26c93112911128f4fa3e61
unknown <poluningm@gmail.com>
2026-07-01 14:56:43 +0300

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pog7x/wallet-service
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,8 @@
+// Package main is the entry point for the application.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world")
+}
